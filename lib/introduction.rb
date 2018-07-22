4205c41d1b23b2efb0a096a2e2162bda3afb1060
oneilld136 <oneilld136@gmail.com>
2018-07-22 00:34:56 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 #introduction_with_language
 
-def introduction with language (name="Josh", language="Ember.js")
+def introduction with language (name="Josh",language="Ember.js")
   puts "Hi, my name is #{name} and I am learning to program in #{language.}
 end
